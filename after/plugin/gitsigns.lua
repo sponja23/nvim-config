e7166d8710434f10e84f4155b21327034243c36c
Sponja- <tomas.spognardi@gmail.com>
2023-01-21 02:37:24 -0300

    docs(gitsigns): Improved documentation on gitsigns' keybinds

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -13,26 +13,27 @@ require("gitsigns").setup {
             if vim.wo.diff then return ']c' end
             vim.schedule(function() gs.next_hunk() end)
             return '<Ignore>'
-        end, { expr = true })
+        end, { expr = true, desc = "Jump to prev hunk" })
 
         map('n', '[c', function()
             if vim.wo.diff then return '[c' end
             vim.schedule(function() gs.prev_hunk() end)
             return '<Ignore>'
-        end, { expr = true })
+        end, { expr = true, desc = "Jump to next hunk" })
 
         -- Actions
-        map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-        map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-        map('n', '<leader>hS', gs.stage_buffer)
-        map('n', '<leader>hu', gs.undo_stage_hunk)
-        map('n', '<leader>hR', gs.reset_buffer)
-        map('n', '<leader>hp', gs.preview_hunk)
+        map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>', { desc = "Stage hunk" })
+        map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>', { desc = "Stage hunk" })
+
+        map('n', '<leader>hS', gs.stage_buffer, { desc = "Stage buffer" })
+        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = "Undo stage hunk" })
+        map('n', '<leader>hR', gs.reset_buffer, { desc = "Reset buffer" })
+        map('n', '<leader>hp', gs.preview_hunk, { desc = "Preview hunk" })
         map('n', '<leader>hb', function() gs.blame_line { full = true } end)
-        map('n', '<leader>tb', gs.toggle_current_line_blame)
+        map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = "Toggle current line blame" })
         map('n', '<leader>hd', gs.diffthis)
         map('n', '<leader>hD', function() gs.diffthis('~') end)
-        map('n', '<leader>td', gs.toggle_deleted)
+        map('n', '<leader>td', gs.toggle_deleted, { desc = "Toggle show deleted hunks" })
 
         -- Text object
         map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
