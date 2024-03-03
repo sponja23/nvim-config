f3de8fe252439cb514977efac97d321fdd66d0c5
sponja23 <tomas.spognardi@gmail.com>
2024-03-03 03:26:58 -0300

    Initial config

---
+++ init.lua
@@ -0,0 +1,69 @@
+-- Set <space> as leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Relative line numbers
+vim.opt.relativenumber = true
+
+-- Enable mouse mode
+vim.opt.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim
+vim.opt.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case insensitive search unless \C or capital letter in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on
+vim.opt.signcolumn = 'yes'
+
+-- Default split directions
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Preview substitutions
+vim.opt.inccommand = 'split'
+
+-- Show line with cursor
+-- vim.opt.cursorline = true
+
+-- Minimum number of lines to keep above/below cursor
+vim.opt.scrolloff = 10
+
+-- Highlight searches
+vim.opt.hlsearch = true
+-- Clear highlighting by pressing escape in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Tab settings
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+
+-- Highlight when copying text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- Install lazy.nvim plugin manager
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup {
+    { import = 'plugins/' }
+}
