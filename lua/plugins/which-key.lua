4662be26281f22e219b2e1a1656a4388d4be3352
sponja23 <tomas.spognardi@gmail.com>
2024-03-25 00:02:33 -0300

    Added which-key plugin

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,11 @@
+-- which-key plugin (see completions on partial keybinds)
+return {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
+    opts = {
+    }
+}
